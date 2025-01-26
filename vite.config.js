5667f3703d33d4bda063582b56673f9c57533f2a
rmx152 <142953687+rmx152@users.noreply.github.com>
2025-01-27 08:31:39 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
-
-
+  base: '/website/', // Replace 'website' with your repository name
+});
