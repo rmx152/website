b1867c2cbd24bc774cb8ea2c2ee578a5dea65411
rmx152 <142953687+rmx152@users.noreply.github.com>
2025-01-27 08:20:42 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
